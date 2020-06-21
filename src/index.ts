a7eb8e43b35be43213eebf2cc34c10acfcd3ae57
mariktar <100trk@gmail.com>
2020-06-21 11:26:49 +0300

    chore(Rotator): corrected logic

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,23 @@
 import * as gm from 'gammacv';
 
+import { rotate } from './rotate';
+
 const imgURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSva5ZvjoZhPmJP6dLVYiP5UzXVRtWkYLjWAt7u684DXNH5cc2y&usqp=CAU';
-const width = 256;
-const height = 256;
+const w = 256;
+const h = 256;
 const wrapper = document.getElementById('container');
 const rangeGrad = document.querySelector('#range_grad') as HTMLElement;
 const valueGrad = document.querySelector('#value_grad');
-const canvas = gm.canvasCreate(width, height);
+const canvas = gm.canvasCreate(w, h);
 wrapper.prepend(canvas);
 
-const render = (g: number) => {
-  gm.imageTensorFromURL(imgURL, 'uint8', [height, width, 4], true).then((input) => {
-    const outputTensor = new gm.Tensor(
-      'uint8',
-      [height, width, 4],
-      new Uint8Array(new Array(height * width * 4).fill(255)),
-    );
-    const rad = g * (Math.PI / 180);
-    const middleW = width / 2;
-    const middleH = height / 2;
-
-    for (let i = 0; i < height; i += 1) {
-      for (let j = 0; j < width; j += 1) {
-        const x = (i - middleH) * Math.cos(rad) - (j - middleW) * Math.sin(rad) + middleH;
-        const y = (i - middleH) * Math.sin(rad) + (j - middleW) * Math.cos(rad) + middleW;
-        const roundX = Math.round(x);
-        const roundY = Math.round(y);
-        const floorX = Math.floor(x);
-        const floorY = Math.floor(y);
-
-        if (roundX < height && roundY < width && roundX >= 0 && roundY >= 0) {
-          for (let rgb = 0; rgb < 3; rgb += 1) {
-            const color = input.get(i, j, rgb);
-            outputTensor.set(roundX, roundY, rgb, color);
-
-            if (roundX !== floorX && floorX < height && floorX >= 0) {
-              outputTensor.set(floorX, roundY, rgb, color);
-            }
-            if (roundY !== floorY && floorY < width && floorY >= 0) {
-              outputTensor.set(roundX, floorY, rgb, color);
-            }
-          }
-        }
-      }
-    }
-    gm.canvasFromTensor(canvas, outputTensor);
+gm.imageTensorFromURL(imgURL, 'uint8', [h, w, 4], true).then((input) => {
+  rangeGrad.addEventListener('input', (e) => {
+    const g = (e.target as HTMLTextAreaElement).value;
+    const rad = +g * (Math.PI / 180);
+    valueGrad.innerHTML = `${g}°`;
+    gm.canvasFromTensor(canvas, rotate(input, rad));
   });
-};
 
-render(0);
-
-rangeGrad.addEventListener('input', (e) => {
-  const g = (e.target as HTMLTextAreaElement).value;
-  valueGrad.innerHTML = `${g}°`;
-  render(+g);
+  gm.canvasFromTensor(canvas, input);
 });
